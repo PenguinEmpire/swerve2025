f824d127afc7237887d42a5a72c7b80cbb586c9f
amanomg8 <amanomg8@gmail.com>
2025-03-26 17:30:08 -0700

    Alex moves block of code

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -210,11 +210,10 @@ public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity) {
     }
      swerveDrive.driveFieldOriented(speeds);
   });
-
-  public Command getAutonomousCommand(String pathName)
-  {
-    // Create a path following command using AutoBuilder. This will also trigger event markers.
-    return new PathPlannerAuto(pathName);
-  }
+}
+public Command getAutonomousCommand(String pathName)
+{
+  // Create a path following command using AutoBuilder. This will also trigger event markers.
+  return new PathPlannerAuto(pathName);
 }
 }
