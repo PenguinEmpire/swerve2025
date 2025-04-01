a6c655274e57bfd54374546401499550122773d4
RehanNazeem <rehannazeem8@gmail.com>
2025-03-31 19:29:40 -0700

    trying to add pid of strafe ( still need to work on it when weight gets added)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,23 +4,18 @@
 
 package frc.robot;
 
-import com.pathplanner.lib.auto.NamedCommands;
+// import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.SwerveDriveCommand;
 import frc.robot.subsystems.SwerveSubsystem;
 import swervelib.SwerveInputStream;
-
-import frc.robot.BooleanReference;
-import frc.robot.commands.SwerveDriveCommand;;
+;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -1,22 +1,12 @@
 package frc.robot.commands;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
-import frc.robot.Constants.OperatorConstants;
 import frc.robot.BooleanReference;
 import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.SwerveSubsystem;
 import swervelib.SwerveInputStream;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import dev.alphagame.LogManager;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class SwerveDriveCommand extends Command {
     SwerveInputStream driveAngularVelocity;
@@ -26,8 +16,13 @@ public class SwerveDriveCommand extends Command {
 
     PIDController rotationPID;
 
+    PIDController strafePID;
+  
+
     public SwerveDriveCommand(SwerveSubsystem swerve, SwerveInputStream input, BooleanReference pressed) {
-        rotationPID = new PIDController(0.005, 0.0, 0.0);
+        // rotationPID = new PIDController(0.02, 0.0, 0.0);
+        strafePID = new PIDController(0.02, 0.0, 0.0);
+
         this.swerveSubsystem = swerve;
         this.driveAngularVelocity = input;
         this.crossPressed = pressed;
@@ -49,16 +44,25 @@ public class SwerveDriveCommand extends Command {
             boolean hasTarget = LimelightHelpers.getTV("limelight");
 
             if (hasTarget) {
-                double tx = LimelightHelpers.getTX("limelight");
+                // double tx = LimelightHelpers.getTX("limelight");
+                double ty = LimelightHelpers.getTY("limelight");
+
                 
-                rotationPID.setSetpoint(0);
-                final double rotPIDVal = clamp(rotationPID.calculate(tx), -0.7, 0.7);
+                // rotationPID.setSetpoint(0);
+                strafePID.setSetpoint(0);
+              
+                // final double rotPIDVal = clamp(rotationPID.calculate(tx), -1.0, 1.0);
+
+                final double StrafePIDVal = clamp(strafePID.calculate(ty), -1.0, 1.0);
 
                 // if the targets exist and the distance is accurate but the robot still goes
                 // away from the target, invert this.
                 boolean pidInvert = false;
-                double rotation = (pidInvert ? -1 : 1) * rotPIDVal;
-                speeds.omegaRadiansPerSecond = rotation;
+                boolean strafeInvert = false;
+                // double rotation = (pidInvert ? -1 : 1) * rotPIDVal;
+                double strafe = (strafeInvert ? -1 : 1) * StrafePIDVal;
+                // speeds.omegaRadiansPerSecond = rotation;
+                speeds.vxMetersPerSecond = strafe;
 
                 SmartDashboard.putBoolean("Apriltag Detected?", true);
             }
