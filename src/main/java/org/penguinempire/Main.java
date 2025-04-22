62798ee7661854be79ca77592942089b67dc4894
Damien Boisvert <damien@alphagame.dev>
2025-04-21 19:31:32 -0700

    feat: Add Rampage, the memory watcher from heck (dev.alphagame.rampage)

--- src/main/java/org/penguinempire/Main.java
+++ src/main/java/org/penguinempire/Main.java
@@ -4,6 +4,7 @@
 
 package org.penguinempire;
 
+import dev.alphagame.rampage.Rampage;
 import dev.alphagame.trailblazer.LogManager;
 import edu.wpi.first.wpilibj.RobotBase;
 
@@ -22,7 +23,9 @@ public final class Main {
    */
   public static void main(String... args) {
     LogManager.info("Starting Robot Program");
-    
+    // -- Note --
+    // Rampage (dev.alphagame.rampage) is a library that provides a memory monitor and other utilities.
+    Rampage.startMemoryMonitor(0.8);
     RobotBase.startRobot(Robot::new);
   }
 }
