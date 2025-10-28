ed6bc0867080559593b93fc0b9df6258246a6fcf
Damien Boisvert <damien@alphagame.dev>
2025-10-27 17:46:44 -0700

    tests: Initializing JUnit Testing Framework

---
+++ src/test/java/org/penguinempire/AlwaysPassing.java
@@ -0,0 +1,11 @@
+package org.penguinempire;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlwaysPassingTest {
+    @Test
+    void alwaysPasses() {
+        assertTrue(true);
+    }
+}
